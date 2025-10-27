1dd98ab171af05bb84d085bfc067e8923b43617f
Kitsor3000 <krostik2007@gmail.com>
2025-10-27 19:40:26 +0200

    Перевірено JWT-аутентифікацію та CRUD API для всіх моделей

--- findme_project/urls.py
+++ findme_project/urls.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # API endpoints
+    # Аутентифікація
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Основні API-маршрути
     path('api/', include('users.urls')),
     path('api/', include('missing_persons.urls')),
     path('api/', include('comments.urls')),
-    path('api/', include('user_messages.urls')),
+    path('api/', include('messages.urls')),
 ]
 
 
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
 from rest_framework import routers
 from .views import UserViewSet, UserProfileViewSet
+from django.urls import path
+from .views_auth import register_user
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'profiles', UserProfileViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('register/', register_user, name='register'),  
+]
+
+urlpatterns += router.urls
